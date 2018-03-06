576e717efc7f2d87eef96eafde4a3cf658da6cb6
prisar <pritimaysarkar2010@gmail.com>
2018-03-06 07:22:54 +0530

    uncomment

--- WebAPIDemo/Controllers/ValuesController.cs
+++ WebAPIDemo/Controllers/ValuesController.cs
@@ -9,17 +9,17 @@ namespace WebAPIDemo.Controllers
 {
     public class ValuesController : ApiController
     {
-        //// GET api/values
-        //public IEnumerable<string> Get()
-        //{
-        //    return new string[] { "value1", "value2" };
-        //}
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
 
-        //// GET api/values/5
-        //public string Get(int id)
-        //{
-        //    return "value";
-        //}
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
 
         // POST api/values
         public void Post([FromBody]string value)
