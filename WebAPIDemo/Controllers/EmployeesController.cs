56d9b476c98bbb4c3a860e3ec308868881c199fa
prisar <pritimaysarkar2010@gmail.com>
2018-03-06 09:49:35 +0530

    Part 4: Add EmployeeDataAccess Class  Library an EF to get Employees data

---
+++ WebAPIDemo/Controllers/EmployeesController.cs
@@ -0,0 +1,29 @@
+﻿using EmployeeDataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAPIDemo.Controllers
+{
+    public class EmployeesController : ApiController
+    {
+        public IEnumerable<Employee> Get()
+        {
+            using (EmployeeDBEntities entites = new EmployeeDBEntities())
+            {
+                return entites.Employees.ToList();
+            }
+        }
+
+        public Employee Get(int id)
+        {
+            using (EmployeeDBEntities entites = new EmployeeDBEntities())
+            {
+                return entites.Employees.FirstOrDefault(e => e.ID == id);
+            }
+        }
+    }
+}
---
+++ WebAPIDemo/Scripts/SQLQuery1.sql
@@ -0,0 +1,19 @@
+﻿use EmployeeDB
+
+
+Create table Employees(
+	ID int primary key identity,
+	FirstName nvarchar(50),
+	LastName nvarchar(50),
+	Gender nvarchar(50),
+	Salary int
+)
+GO
+
+Insert into Employees values('Mark', 'Hastings', 'Male', 60000)
+Insert into Employees values('Steve', 'Pound', 'Male', 45000)
+Insert into Employees values('Ben', 'Hoskins', 'Male', 70000)
+Insert into Employees values('Philip', 'Hastings', 'Male', 45000)
+Insert into Employees values('Mary', 'Lambeth', 'Female', 30000)
+Insert into Employees values('Valarie', 'Vikings', 'Female', 35000)
+Insert into Employees values('Jahn', 'Stanmore', 'Male', 80000)
