1f61edf5fcfe74eea2a8912ce08072b9be7a24a2
prisar <pritimaysarkar2010@gmail.com>
2018-03-06 13:07:34 +0530

    Part 7: Implementing Post method

--- WebAPIDemo/Controllers/EmployeesController.cs
+++ WebAPIDemo/Controllers/EmployeesController.cs
@@ -18,11 +18,41 @@ namespace WebAPIDemo.Controllers
             }
         }
 
-        public Employee Get(int id)
+        public HttpResponseMessage Get(int id)
         {
             using (EmployeeDBEntities entites = new EmployeeDBEntities())
             {
-                return entites.Employees.FirstOrDefault(e => e.ID == id);
+                var entity =  entites.Employees.FirstOrDefault(e => e.ID == id);
+
+                if (entity != null)
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, entity);
+                }
+                else
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Employee with Id = " + id.ToString() + " not found");
+                }
+            }
+        }
+
+        public HttpResponseMessage Post([FromBody] Employee employee)
+        {
+            try
+            {
+                using (EmployeeDBEntities entites = new EmployeeDBEntities())
+                {
+                    entites.Employees.Add(employee);
+                    entites.SaveChanges();
+
+                    var message = Request.CreateResponse(HttpStatusCode.Created, employee);
+                    message.Headers.Location = new Uri(Request.RequestUri + "/" + employee.ID.ToString());
+
+                    return message;
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
             }
         }
     }
